78ae553b37a5e952d697ecfe3328289dec04b6b6
jasmax <71804417+jasmax@users.noreply.github.com>
2023-08-21 11:06:06 +0800

    Update autoNaviSelenium.py

--- autoNaviSelenium.py
+++ autoNaviSelenium.py
@@ -3,9 +3,6 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 
 class Page(object):
     def __init__(self, driver):
